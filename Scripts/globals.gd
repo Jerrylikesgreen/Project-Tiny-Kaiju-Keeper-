ca2dee600d3c4a0644f9684624ca953453daf1c4
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-06-28 08:17:59 -0400

    Initial Commit.
    Set up base body, for all Mobs to use. Set up default sprite for base body along with VPet resource to track stats and player data. ALso set up BGM - currently set egg theme on loop. I also set up a very basic title screen.

---
+++ Scripts/globals.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+var current_happieness:float
+var current_hunger:float
+var current_hygiene:float
---
+++ Scripts/pet_resource_script.gd
@@ -0,0 +1,9 @@
+class_name PetResource extends Resource
+
+
+@export var happiness: float = 1.0 
+@export var hunger: float = 1.0
+@export var hygiene: float = 1.0
+
+
+@export var sprite: CompressedTexture2D
