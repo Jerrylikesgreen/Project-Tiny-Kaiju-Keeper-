771a1a47a170552605f95943be856c5ef1131602
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-07-01 07:20:36 -0400

    Add user authentication middleware
    Introduced a new middleware to handle user authentication for protected routes. This ensures that only authenticated users can access certain endpoints, improving application security.

--- Scenes/sfx.gd
+++ Scenes/sfx.gd
@@ -1,34 +1,30 @@
-class_name BGM extends AudioStreamPlayer
-## Node that controls Background music for thhe game, depending on Game State and other variables. 
-
+class_name SFX extends AudioStreamPlayer
 
 
 enum Track { ## [ Enum: LOGICAL_NAME → int ]  Refrences the differant tracks Player can play. 
-	EGG_1,                   ## 0
-	BABY_1,                  ## 1
-	TODDLER_1,               ## 2
-	JUVENILE_1,              ## 3 
-	ADULT_1,                 ## 4
-	NAPTIME_SWEET_KAIJU,     ## 5
-	THEY_GROW_UP_SO_FAST_1   ## 6
+	EVOLUTION,                   ## 0
+	BLIP,
+	CHIRP,
+	FLAP,
+	MUNCH_1,
+	MUNCH_2,
+	ROAR
 }
+
 
 ## [ Lookup table: enum value → AudioStream ] Refrence to each Path for each Track. 
 const TRACK_STREAMS := {
-	Track.ADULT_1:                 preload("res://Assets/OGG/Adult (1).ogg"),
-	Track.BABY_1:                  preload("res://Assets/OGG/Baby (1).ogg"),
-	Track.EGG_1:                   preload("res://Assets/OGG/Egg (1).ogg"),
-	Track.JUVENILE_1:              preload("res://Assets/OGG/Juvenile (1).ogg"),
-	Track.NAPTIME_SWEET_KAIJU:     preload("res://Assets/mp3/Naptime, sweet kaiju.mp3"),
-	Track.THEY_GROW_UP_SO_FAST_1:  preload("res://Assets/mp3/They grow up so fast (1).ogg"),
-	Track.TODDLER_1:               preload("res://Assets/OGG/Toddler (1).ogg")
+	Track.EVOLUTION: preload("res://Assets/OGG/Upgrade.ogg"),
+	Track.BLIP:  preload("res://Assets/OGG/Blip.ogg"),
+	Track.CHIRP: preload("res://Assets/OGG/Chirp Chirp.ogg"),
+	Track.FLAP: preload("res://Assets/OGG/Flap.ogg"),
+	Track.MUNCH_1: preload("res://Assets/OGG/Munch 1.ogg"),
+	Track.MUNCH_2: preload("res://Assets/OGG/Munch 2.ogg"),
+	Track.ROAR: preload("res://Assets/OGG/Roar.ogg")
+
 }
 
 var _current_track : Track        ## remember what’s playing
-
-func _ready() -> void:
-	play_track(Track.NAPTIME_SWEET_KAIJU) ## Ensures audio gets played at start. 
-	Events.game_state.connect(_update_track)
 
 
 
@@ -41,11 +37,12 @@ func play_track(track: Track, fade_time: float = 1.0) -> void:  ## Function that
 	else:
 		_switch_stream(track)
 		_fade_in(fade_time)
-func _update_track()->void: ## Function that calls pl_track and assigns Track based on current_game_state
-	if Globals.current_game_state == 0: ## GUI 
-		play_track(Track.NAPTIME_SWEET_KAIJU)
-	if Globals.current_game_state == 1: ## MAIN
-		play_track(Track.EGG_1)
+		
+##func _update_track()->void: ## Function that calls pl_track and assigns Track based on current_game_state
+##	if Globals.current_game_state == 0: ## GUI 
+##		play_track(Track.NAPTIME_SWEET_KAIJU)
+##	if Globals.current_game_state == 1: ## MAIN
+##		play_track(Track.EGG_1)
 
 func _fade_out_then_switch(new_track: Track, t: float) -> void:## {PRIVATE} Helper to fade bgm  
 	var tw := get_tree().create_tween() ## Create tween
